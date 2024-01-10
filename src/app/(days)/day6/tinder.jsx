654605760bb5cfed7eb5d5cb8a386840d0d3aa4f
Samad Musiliu <musiliusamad@gmail.com>
2024-01-10 23:57:49 +0100

    ...

--- src/app/(days)/day6/tinder.jsx
+++ src/app/(days)/day6/tinder.jsx
@@ -6,9 +6,20 @@ import TinderCard from '../../../components/day6/TinderCard';
 export default function TinderScreen() {
 
     const users = [
-        { id: 1, name: 'John', age: 25, image: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600' },
-        { id: 2, name: 'Emma', age: 23, image: 'https://images.pexels.com/photos/4612159/pexels-photo-4612159.jpeg?auto=compress&cs=tinysrgb&w=600' },
-        { id: 3, name: 'Michael', age: 27, image: 'https://images.pexels.com/photos/1559086/pexels-photo-1559086.jpeg?auto=compress&cs=tinysrgb&w=600' },
+        {
+            id: 1,
+            name: 'John',
+            age: 25,
+            image: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600'
+        },
+        { id: 
+            2, 
+            name: 'Emma', 
+            age: 23, 
+            image: 'https://images.pexels.com/photos/4612159/pexels-photo-4612159.jpeg?auto=compress&cs=tinysrgb&w=600' },
+        { id: 3, 
+            name: 'Michael', 
+            age: 27, image: 'https://images.pexels.com/photos/1559086/pexels-photo-1559086.jpeg?auto=compress&cs=tinysrgb&w=600' },
         { id: 4, name: 'Rose', age: 27, image: 'https://images.pexels.com/photos/9956077/pexels-photo-9956077.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' },
     ]
 
